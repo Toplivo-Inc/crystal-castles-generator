a1907c5d0d12fd0a92e909a5d8e246816b8fc0bf
rgimad <33692565+rgimad@users.noreply.github.com>
2025-10-17 15:33:06 +0300

    рефакторинг функционала отладочного окна чтобы смотреть до и после обработки

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,41 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+use std::cmp;
 use image::ImageReader;
-
 use macroquad::{miniquad::window::set_window_size, prelude::*};
+
+pub trait FromAnyImageFile {
+    fn from_any_image_file(fname: &str) -> Texture2D;
+}
+
+impl FromAnyImageFile for Texture2D {
+    fn from_any_image_file(fname: &str) -> Texture2D {
+        let img = ImageReader::open(fname)
+            .expect("failed to open")
+            .decode()
+            .expect("failed to decode");
+
+        let rgba_img = img.to_rgba8();
+        let (image_width, image_height) = rgba_img.dimensions();
+        let decoded_bytes = rgba_img.into_raw();
+        Texture2D::from_rgba8(image_width as u16, image_height as u16, &decoded_bytes)
+    }
+}
 
 #[macroquad::main("window 1")]
 async fn main() {
-    let img = ImageReader::open("samples/anon.jpg")
-        .expect("failed to open")
-        .decode()
-        .expect("failed to decode");
+
+    let texture1 = Texture2D::from_any_image_file("samples/do.png");
+    let texture2 = Texture2D::from_any_image_file("samples/posle.png");
     
-    // Convert to RGBA if needed
-    let rgba_img = img.to_rgba8();
-    let (image_width, image_height) = rgba_img.dimensions();
-    
-    // Get the raw bytes
-    let decoded_bytes = rgba_img.into_raw();
-    
-    // Create texture from RGBA bytes
-    let texture = Texture2D::from_rgba8(image_width as u16, image_height as u16, &decoded_bytes);
-    
-    set_window_size(image_width, image_height);
+    set_window_size(texture1.width() as u32 + texture2.width() as u32, cmp::max(texture1.height() as u32, texture2.height() as u32));
     
     loop {
         clear_background(WHITE);
         
-        draw_texture(&texture, 0.0, 0.0, WHITE);
+        draw_texture(&texture1, 0.0, 0.0, WHITE);
+        draw_texture(&texture2, texture1.width(), 0.0, WHITE);
         
         next_frame().await;
     }
